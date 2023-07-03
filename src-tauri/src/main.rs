174708f2bd58a82c97f1182e02d77acac3591b01
Anthony Bellancourt <anthony.bellancourt@leviia.com>
2023-07-03 20:36:40 +0200

    Add greeting command to main.rs
    Added a `greet` command to the `main.rs` file using the `#[tauri::command]` attribute. The `greet` function is an asynchronous function that takes a `name` parameter and returns a formatted greeting string.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+async fn greet(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
