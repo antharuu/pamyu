eed5534a58fd19aeadabeeda2b017494404cbbbc
Anthony Bellancourt <anthony.bellancourt@leviia.com>
2023-07-05 22:06:18 +0200

    src-tauri: Add list_res command to handle listing resources
    This commit adds a new command `list_res` to handle listing resources in a specified directory. The `list_res` command uses the `fs::read_dir` function to read the contents of the directory, and then iterates over the entries, collecting the file names into a vector. If any invalid UTF-8 sequence is encountered in a file name, the command returns an error with the message "Invalid UTF-8 sequence in file name". If there is any other error reading the directory, the command returns the corresponding error message.
    
    The `list_res` command is registered as an invoke handler in the `main` function of the Tauri application.
    
    Note: This commit also includes modifications to the `Cargo.toml` file to add the "path-all" feature to the `tauri` dependency.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -32,9 +32,38 @@ async fn save(version: String, value: i32) -> bool {
     false
 }
 
+#[tauri::command]
+async fn list_res(path: String) -> Result<Vec<String>, String> {
+    use std::fs;
+
+    match fs::read_dir(path) {
+        Ok(entries) => {
+            let mut files = Vec::new();
+            for entry in entries {
+                match entry {
+                    Ok(entry) => match entry.file_name().to_str() {
+                        Some(file_name) => {
+                            files.push(file_name.to_owned());
+                        }
+                        None => {
+                            return Err(format!("Invalid UTF-8 sequence in file name"));
+                        }
+                    },
+                    Err(error) => {
+                        return Err(error.to_string());
+                    }
+                }
+            }
+            Ok(files)
+        }
+        Err(error) => Err(error.to_string()),
+    }
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![save])
+        .invoke_handler(tauri::generate_handler![list_res])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
