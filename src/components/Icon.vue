39b4e6c7a4f5ef5127ac861a9640f0bdb338d621
Anthony Bellancourt <antho.eclips@gmail.com>
2023-09-19 22:41:16 +0200

    icon

---
+++ src/components/Icon.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+</script>
+
+<template>
+  <i class="material-symbols-outlined" v-text="name.trim()"></i>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined');
+</style>
