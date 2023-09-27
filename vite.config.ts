09e1b24f936f9db2cee60c8e4ba414749d924f1c
Anthony Bellancourt <antho.eclips@gmail.com>
2023-09-27 13:46:12 +0200

    some conf for alpha 7

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,27 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import vue from '@vitejs/plugin-vue';
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [
-      vue(),
-      VueI18nPlugin({})
-  ],
+    plugins: [
+        vue(),
+        VueI18nPlugin({})
+    ],
+    build: {
+        target: 'esnext',
+    },
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-  },
-  // 3. to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent vite from obscuring rust errors
+    clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    server: {
+        port: 1420,
+        strictPort: true,
+    },
+    // 3. to make use of `TAURI_DEBUG` and other env variables
+    // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+    envPrefix: ['VITE_', 'TAURI_'],
 }));
