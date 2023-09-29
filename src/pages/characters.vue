e6a887a8007d5ffa0cde05c0332d897b8531586c
Anthony Bellancourt <antho.eclips@gmail.com>
2023-09-29 17:20:25 +0200

    can now delete folders

--- src/pages/characters.vue
+++ src/pages/characters.vue
@@ -5,6 +5,7 @@ import {useCharacterStore} from '../stores/characterStore.ts';
 
 import ActionButton from '../components/ActionButton.vue';
 import CharacterListElement from '../components/CharacterListElement.vue';
+import Icon from '../components/Icon.vue';
 
 import ListContainer from '../layout/ListContainer.vue';
 import PageLayout from '../layout/PageLayout.vue';
@@ -12,7 +13,9 @@ import PageLayout from '../layout/PageLayout.vue';
 const characters = computed(() => useCharacterStore().getCharacters);
 const folders = computed(() => useCharacterStore().getCharactersFolders);
 
-console.log(folders.value);
+function delete_folder(name: string): void {
+    useCharacterStore().deleteFolder(name);
+}
 </script>
 
 <template>
@@ -49,6 +52,14 @@ console.log(folders.value);
                   class="folder__title"
                 >
                   {{ folder !== '__others' ? folder : $t('others') }}
+                  <div class="folder__title__actions">
+                    <span
+                      v-if="folder !== '__others'"
+                      @click="delete_folder(folder)"
+                    >
+                      <Icon name="delete" />
+                    </span>
+                  </div>
                 </div>
                 <div class="folder__characters">
                   <div
@@ -118,11 +129,35 @@ console.log(folders.value);
         }
 
         &__title {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
             letter-spacing: .075rem;
             font-size: 1.2rem;
             text-transform: uppercase;
             color: var(--color-accent);
-            margin-bottom: 1rem
+            margin-bottom: 1rem;
+            cursor: default;
+
+            &__actions {
+                opacity: 0;
+                display: flex;
+                align-items: center;
+                gap: .5rem;
+                margin-left: auto;
+                font-size: .25rem;
+                transition: opacity .2s ease-in-out;
+
+                span {
+                    cursor: pointer;
+                }
+            }
+
+            &:hover {
+                .folder__title__actions {
+                    opacity: 1;
+                }
+            }
         }
 
         &__characters {
